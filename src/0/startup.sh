e1a25a3e8f48a336043d97b0a55c5a722412caff
ytyaru <ytyaru@outlook.jp>
2021-01-28 09:35:25 +0900

    Uncaught TypeError: n(...) is not a function

--- src/0/startup.sh
+++ src/0/startup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 npm init -y
-npm i -D webpack webpack-cli webpack-dev-server babel-loader @babel/core @babel/preset-env @babel/plugin-proposal-class-properties @babel/plugin-proposal-private-methods @babel/runtime @babel/plugin-transform-runtime
+npm i -D webpack webpack-cli webpack-dev-server babel-loader @babel/core @babel/preset-env @babel/plugin-proposal-class-properties @babel/plugin-proposal-private-methods @babel/runtime @babel/plugin-transform-runtime @babel/plugin-transform-regenerator core-js@3 @babel/runtime-corejs3
 # private # : @babel/plugin-proposal-class-properties @babel/plugin-proposal-private-methods 
 # async     : @babel/runtime @babel/plugin-transform-runtime
+#           : core-js@3 なお、 @babel/runtime-corejs3 だけではエラーが出た。両方必要かは不明。
+# yield     : @babel/plugin-transform-regenerator
 npm i 
 npm i --save-dev
 npx webpack
--- src/0/webpack.config.js
+++ src/0/webpack.config.js
@@ -21,15 +21,37 @@ module.exports = {
             options: {
               presets: [
                 // ES2020 を ES5 に変換
-                "@babel/preset-env",
+                ["@babel/preset-env", {
+                    useBuiltIns: "usage",
+                    corejs: 3,
+//                    modules: 'false',//commonjs,amd,umd,systemjs,auto
+                    modules: 'umd',//commonjs,amd,umd,systemjs,auto
+                }]
               ],
               plugins: [
                 // private # を使えるようにする
                 "@babel/plugin-proposal-class-properties",
                 "@babel/plugin-proposal-private-methods",
                 // asyncを使えるようにする
-                "@babel/plugin-transform-runtime",
+//                "@babel/plugin-transform-runtime",
+                ["@babel/plugin-transform-runtime",{
+                    corejs: 3,
+                    regenerator: true,   
+                }],
+                // yieldを使えるようにする
+                "@babel/plugin-transform-regenerator",
               ],
+              /*
+              plugins: [
+                // private # を使えるようにする
+                "@babel/plugin-proposal-class-properties",
+                "@babel/plugin-proposal-private-methods",
+                // asyncを使えるようにする
+                "@babel/plugin-transform-runtime",
+                // yieldを使えるようにする
+                "@babel/plugin-transform-regenerator",
+              ],
+              */
             },
           },
         ],
